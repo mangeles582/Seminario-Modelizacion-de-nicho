ae27dccf62ae4463780aaa8a663ef658b55ba546
mangeles582 <mangeles582@users.noreply.github.com>
2017-02-22 16:11:02 +0100

    Update funcionesSDM_taller1.R

--- taller 1/funcionesSDM_taller1.R
+++ taller 1/funcionesSDM_taller1.R
@@ -42,5 +42,21 @@ repeat{
 return(data)
 
 }
+  
+  
+  Dsquared <- function(model, adjust = TRUE) {
+  # version 1.1 (13 Aug 2013)
+  # calculates the explained deviance of a GLM
+  # model: a model object of class "glm"
+  # adjust: logical, whether or not to use the adjusted deviance taking into acount the nr of observations and parameters (Weisberg 1980; Guisan & Zimmermann 2000)
+  d2 <- (model$null.deviance - model$deviance) / model$null.deviance
+  if (adjust) {
+    n <- length(model$fitted.values)
+    p <- length(model$coefficients)
+    d2 <- 1 - ((n - 1) / (n - p)) * (1 - d2)
+  }
+  return(d2)
+}  # end Dsquared function
+
 
 
